aa4bf456de67e15259e4710d87f622ecf737008d
Hassan Motiei <123471966+HassanMotiei@users.noreply.github.com>
2024-04-12 11:54:27 +0330

    add Features and Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://72371e638f6b64ce70099760ff6a62ef@o4507058333745152.ingest.us.sentry.io/4507069909499904',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
